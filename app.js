934648eedc8cc4a4fa623a20d43c4840e264dfbb
Larry-nan <lawrence,yinan@gmail.com>
2019-10-28 18:17:01 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    var firebaseConfig = {
+        apiKey: "AIzaSyASorbtxPo3XGAFFw6y8Epb-Kl-hcf_1wY",
+        authDomain: "trainschedule-684aa.firebaseapp.com",
+        databaseURL: "https://trainschedule-684aa.firebaseio.com",
+        projectId: "trainschedule-684aa",
+        storageBucket: "trainschedule-684aa.appspot.com",
+        messagingSenderId: "45141282344",
+        appId: "1:45141282344:web:f068da74d12af63c82af1d"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+})
