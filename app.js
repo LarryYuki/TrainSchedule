b6bcb3ff755ba0329baa5f155d3f0aacb37a08e4
Larry-nan <lawrence,yinan@gmail.com>
2019-10-30 15:57:42 -0700

    added html, db input

--- app.js
+++ app.js
@@ -10,4 +10,50 @@ $(document).ready(function () {
     };
     firebase.initializeApp(firebaseConfig);
 
-})
+    var db = firebase.database()
+
+    $('#submit').on('click', function (e) {
+        e.preventDefault();
+        var trainName = $('#trainName').val().trim()
+        var destination = $('#dest').val().trim()
+        var firstTrainTime = $('#first').val().trim()
+        var frequency = $('#frequency').val().trim()
+
+        var train = {
+            name: trainName,
+            dest: destination,
+            time: firstTrainTime,
+            frequency: frequency,
+        }
+        console.log(train)
+        db.ref().push(train)
+
+    })
+    db.ref().on("child_added", function (snapshot) {
+        console.log(snapshot.val())
+        console.log(snapshot.key) //if we need the key firebase creates on push
+        var thisTrain = snapshot.val()
+
+        var newRow = $('<tr>')
+        //no chaining
+        var newCell = $('<td>')
+        newCell.text(thisTrain.name)
+        newRow.append(newCell)
+        //chaining
+        newRow.append($('<td>').text(thisTrain.dest))
+
+
+
+        //put on page
+        $('tbody').append(newRow)
+
+
+    })
+
+});
+//moment notes
+console.log(moment)
+moment() //= now
+let noon = '12:00'
+console.log(moment(noon, "hh:mm")) // the string inside moment is how you parse the noon string
+console.log(moment(noon, "MM:YY")) // the string inside moment is how you parse the noon string, now as months and years
